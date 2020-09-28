73701b6463850d5defceb6498c5bf5cd23d82474
wei50o7 <61506795+wei50o7@users.noreply.github.com>
2020-09-28 16:55:41 +0800

    Add files via upload

---
+++ calendar/test.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/test.css">
+    <script src="js/test.js"></script>
+    <title></title>
+  </head>
+  <body>
+        <div class="top-container">
+          <div class="left-container">
+            <div class="left-img">
+              <img src="../img/salmon.jpg" alt="">
+            </div>
+          </div>
+          <div class="right-container">
+            <div class="product-title">
+              <p>Chicken Rice</p>
+            </div>
+            <div class="close">
+              <button type="button" name="button"></button>
+            </div>
+            <div class="product-desc">
+              <p>esse quid quem sunt eram export illum fugiat enim enim fore nulla quae irure
+                legam export aute enim tempor quorum fore aute noster quorum anim irure ipsum
+                export quem aliqua quis fore multos fugiat quis duis quorum anim nulla legam
+                duis illum dolore dolore fore anim dolor enim dolore duis</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="nutri-container">
+          <div class="price">
+            <p>Total Price : RM50</p>
+          </div>
+          <div class="nutri-table">
+            <table>
+          <tr>
+            <th>Calories</th>
+            <th>69</th>
+          </tr>
+          <tr>
+            <th>Carbohydrates</th>
+            <th>69</th>
+          </tr>
+          <tr>
+            <th>Protein</th>
+            <th>69</th>
+          </tr>
+          <tr>
+            <th>Fats</th>
+            <th>69</th>
+          </tr>
+        </table>
+          </div>
+
+        </div>
+        <div class="addon-container">
+          <div class="addon">
+            <div class="addon-left">
+              <p>Salmon Steak</p>
+            </div>
+            <div class="addon-right">
+              <button type="submit" name="button" class="left-btn">+</button>
+              <p>150g</p>
+              <button type="submit" name="button" class="right-btn">-</button>
+            </div>
+          </div>
+          <div class="addon">
+            <div class="addon-left">
+              <p>Salmon Steak</p>
+            </div>
+            <div class="addon-right">
+              <button type="submit" name="button" class="left-btn">+</button>
+              <p>150g</p>
+              <button type="submit" name="button" class="right-btn">-</button>
+            </div>
+          </div>
+          <div class="addon">
+            <div class="addon-left">
+              <p>Salmon Steak</p>
+            </div>
+            <div class="addon-right">
+              <button type="submit" name="button" class="left-btn">+</button>
+              <p>150g</p>
+              <button type="submit" name="button" class="right-btn">-</button>
+            </div>
+          </div>
+        </div>
+        <div class="datepicker-container">
+        </div>
+        <div class="bottom-container">
+          <div class="button-container">
+            <div class="button-position">
+              <button type="submit" name="button">Add to cart</button>
+            </div>
+          </div>
+          <div class="datepicker-container">
+            <div class="datepicker-center">
+              <?php
+              include 'index-multi.html';
+              ?>
+            </div>
+          </div>
+        </div>
+
+  </body>
+</html>
